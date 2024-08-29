f4bbd5b5a1fa2af369bc7ad3d6936cebcd72a74f
Tran Van Cong <aanh7108@gmail.com>
2024-08-29 23:57:32 +0700

    tinh nang tim kiem

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -20,15 +20,30 @@ module.exports.index = async (req, res) => {
     const find = {
         deleted: false
     }
+
+    //bo loc
     if (req.query.status) {
         find.status = req.query.status
     }
+    //end bo loc
+
+    //sap xep theo nhieu tieu chi
     const sortKey = req.query.sortKey;
     const sortValue = req.query.sortValue;
     const sort = {};
     if (sortKey && sortValue) {
         sort[sortKey] = sortValue;
     }
+    //end
+
+    // tim kiem
+    let keyword = "";
+    if (req.query.keyword) {
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
+    }
+    //end tim kiem
+
     const task = await taskDtb.find(find)
         .sort(sort)
         .skip(skip.skipPage)
