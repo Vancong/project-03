c4aeb5c229457be794ada92557180f82da76e71e
Tran Van Cong <aanh7108@gmail.com>
2024-09-01 13:55:42 +0700

    cai dat thu vien cors

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 require('dotenv').config();
 const app = express()
 const port = 4000
+
+// app.use(cors()) // cach 1 tat ca cac ten mien duoc truy cap
+
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+} // cach 2 cu the ten mien 
+app.use(cors(corsOptions))
 
 const database = require('./config/database.config.js');
 database.connect();
